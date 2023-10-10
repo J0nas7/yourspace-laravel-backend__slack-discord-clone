2f42f2b286f2b755283335428539963a9a7c82e6
J0nas7 <jonas.sorensen.93dk@gmail.com>
2023-10-10 16:50:02 +0200

    Create space

---
+++ app/Http/Controllers/SpaceController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Space;
+use App\Helpers\DataService;
+use Illuminate\Support\Facades\Auth;
+
+use DateTime;
+
+class SpaceController extends Controller
+{
+    private $request;
+    private $searchTerm;
+    private $pageNr;
+
+    // Instantiate a new controller instance
+    public function __construct(Request $request) {
+        $this->request = $request;
+        $this->searchTerm = json_decode($this->request->input('postContent'))->searchTerm ?? null;
+        $this->pageNr = json_decode($this->request->input('postContent'))->pageNr ?? null;
+    }
+
+    // Create a new space
+    public function createNewSpace() {
+        $createFailed = false;
+        $errorMsg = "";
+
+        $SpaceName = $this->request->Space_Name;
+
+        if (empty($this->request->Space_Name)) {
+            $createFailed = true;
+            $errorMsg = "Missing space name";
+        }
+        
+        // Check that Space Name is not occupied
+        $nameOccupied = Space::where("Space_Name", $SpaceName)->first();
+        if ($nameOccupied) {
+            $createFailed = true;
+            $errorMsg = "The space name is already taken.";
+        }
+
+        if (!$createFailed) {
+            $createdAt = new DateTime();
+            $createdAt = $createdAt->format('d/m-Y H:i:s'); 
+            $space = Space::create([
+                'Space_Name' => $SpaceName,
+                'Space_ImageUrl' => $this->request->Space_ImageUrl ?? '',
+                'Space_InviteCode' => '',
+                'Space_ProfileID' => Auth::user()->Profile_ID ?? 1
+            ]);
+        }
+        
+        if (!$createFailed && $space) {
+            return response()->json([
+                'success' => true,
+                'message' => 'The space was created',
+                'data'    => true
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => (!empty($errorMsg) ? $errorMsg : 'Space Creation Failed '),
+                'data'    => false
+            ], 200);
+        }
+    }
+}
